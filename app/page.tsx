8df841c87c53153ebf19dd9d7d7d695704d4e0dd
Asada678 <asada.tomoya0923@gmail.com>
2023-02-27 21:17:46 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 
-import PostList from "./components/post/post-list";
+// import PostList from "./components/post/post-list";
 import Loading from "./loading";
 import PostNew from "./components/post/post-new";
+
+const PostList = React.lazy(() => import("./components/post/post-list"));
 
 export default function Page() {
   return (
     <div className="h-full">
       <Suspense fallback={<Loading />}>
-        {/* @ts-expect-error Server Component */}
         <PostList />
       </Suspense>
       <PostNew />
