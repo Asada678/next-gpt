8e028e5664f754c74ca9fe5bc2498dc7433461b1
Asada678 <asada.tomoya0923@gmail.com>
2023-02-27 21:04:28 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ export default function Page() {
   return (
     <div className="h-full">
       <Suspense fallback={<Loading />}>
-        {/* @ts-ignore */}
         <PostList />
       </Suspense>
       <PostNew />
